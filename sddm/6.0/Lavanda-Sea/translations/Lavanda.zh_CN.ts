8d18977b52fe12b5f799f08c2406736ec9eca51a
Wenyin Root <ruojiner@hotmail.com>
2026-05-14 02:05:27 +0800

    Add zh_CN translations for Lavanda SDDM themes

---
+++ sddm/6.0/Lavanda-Sea/translations/Lavanda.zh_CN.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Main</name>
+    <message>
+        <source>Caps Lock is on</source>
+        <translation>大写锁定已开启</translation>
+    </message>
+    <message>
+        <source>Sleep</source>
+        <translation>睡眠</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>重启</translation>
+    </message>
+    <message>
+        <source>Shut Down</source>
+        <translation>关机</translation>
+    </message>
+    <message>
+        <source>Other…</source>
+        <translation>其他用户…</translation>
+    </message>
+    <message>
+        <source>Type in Username and Password</source>
+        <translation>输入用户名和密码</translation>
+    </message>
+    <message>
+        <source>List Users</source>
+        <translation>切换用户</translation>
+    </message>
+    <message>
+        <source>Virtual Keyboard</source>
+        <translation>虚拟键盘</translation>
+    </message>
+    <message>
+        <source>Login Failed</source>
+        <translation>登录失败</translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <source>Username</source>
+        <translation>用户名</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+</context>
+<context>
+    <name>SessionButton</name>
+    <message>
+        <source>Desktop Session: %1</source>
+        <translation>桌面会话：%1</translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardButton</name>
+    <message>
+        <source>Keyboard Layout: %1</source>
+        <translation>键盘布局：%1</translation>
+    </message>
+</context>
+</TS>
